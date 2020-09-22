adb8f9963fbf0edbcb2f933e7f850d1aa9de7035
Ansul Thakur <ansultkr@iitk.ac.in>
2020-09-22 21:24:02 +0530

    2:@Configuration & @ComponentScan & beans.xml eliminated

--- src/com/ansul/thakur/Client.java
+++ src/com/ansul/thakur/Client.java
@@ -1,13 +1,14 @@
 package com.ansul.thakur;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 
 public class Client {
 	public static void main(String[] args) {
-		ApplicationContext context= new ClassPathXmlApplicationContext("beans.xml");
+		ApplicationContext context= new AnnotationConfigApplicationContext(CollegeConfig.class);
 		College college =context.getBean("collegeBean",College.class);
-		System.out.println("College Object created");
+		System.out.println("College Object created at:: "+college);
 	}
 
 }
---
+++ src/com/ansul/thakur/CollegeConfig.java
@@ -0,0 +1,10 @@
+package com.ansul.thakur;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.ansul.thakur")
+public class CollegeConfig {
+
+}
