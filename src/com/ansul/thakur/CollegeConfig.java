44846200dd161f9cc9c9d92384b9fb3c327f1a51
Ansul Thakur <ansultkr@iitk.ac.in>
2020-09-22 21:40:07 +0530

    4:@bean another example/way

--- src/com/ansul/thakur/CollegeConfig.java
+++ src/com/ansul/thakur/CollegeConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class CollegeConfig {
 	
-	@Bean
+	@Bean(name="colbean")
 	public College collegeBean() {
 		return new College();
 	}
