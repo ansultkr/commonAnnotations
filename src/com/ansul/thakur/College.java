203e1d33f59a08c9d857e9d3a04da1f0a603df60
Ansul Thakur <ansultkr@iitk.ac.in>
2020-10-04 11:12:16 +0530

    15: @Qualifier for conflict resolution but remember to use first letter small

--- src/com/ansul/thakur/College.java
+++ src/com/ansul/thakur/College.java
@@ -1,6 +1,7 @@
 package com.ansul.thakur;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +14,7 @@ public class College {
 	private Principal principal;
 	
 	@Autowired
+	@Qualifier("mathTeacher")
 	private Teacher teacher;
 	
 	
